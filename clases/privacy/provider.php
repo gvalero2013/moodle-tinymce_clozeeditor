8761186b63f8a37b5ba78e06eb97bbb1f104d75f
gvalero2013 <40007744+gvalero2013@users.noreply.github.com>
2018-06-07 04:22:50 -0500

    rewrite comments

--- clases/privacy/provider.php
+++ clases/privacy/provider.php
@@ -14,25 +14,32 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 // *
- + // Privacy Subsystem implementation for tinyMCE_cloze.
- + //
- + // @package    tinymce_clozeeditor
- + // @copyright  2018 German Valero <gvalero@unam.mx>
- + // @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- + // .
- + namespace tinymce_clozeeditor\privacy;
-+ defined('MOODLE_INTERNAL') || die();
-+ class provider implements
-    // This plugin does not store any personal user data.
+// Privacy Subsystem implementation for tinyMCE_cloze.
+//
+// @package    tinymce_clozeeditor
+// @copyright  2018 German Valero <gvalero@unam.mx>
+// @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+// .
+namespace tinymce_clozeeditor\privacy;
+defined('MOODLE_INTERNAL') || die();
+//
+// Privacy Subsystem for tinymce_mathslate implementing null_provider.
+//
+// @copyright   2018 Daniel Thies <dethies@gmail.com>
+// @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+// .
+
+class provider implements
+
     \core_privacy\local\metadata\null_provider
 {
     use \core_privacy\local\legacy_polyfill;
-    /**
-     * Get the language string identifier with the component's language
-     * file to explain why this plugin stores no data.
-     *
-     * @return  string
-     */
+//
+// Get the language string identifier with the component's language
+// file to explain why this plugin stores no data.
+//
+// @return  string
+//
     public static function _get_reason() {
         return 'privacy:null_reason';
     }
