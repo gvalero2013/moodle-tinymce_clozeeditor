154228e7a32a77d2c17a7f2d95f0944f2d96ce91
gvalero2013 <40007744+gvalero2013@users.noreply.github.com>
2018-09-05 19:40:15 -0500

    copied from moodle.com

--- tinymce/editor_plugin.js
+++ tinymce/editor_plugin.js
@@ -24,25 +24,25 @@
                  * @param {tinymce.Editor} ed Editor instance that the plugin is initialized in.
                  * @param {string} url Absolute URL to where the plugin is located.
                  */
-            init: function(ed, url) {
+            init : function(ed, url) {
                         // Register the command so that it can be invoked by using tinyMCE.activeEditor.execCommand('mceclozeeditor'); .
                         ed.addCommand('mceclozeeditor', function() {
                             lang = ed.getParam('language');
                                 ed.windowManager.open({
-                                    file: ed.getParam("moodle_plugin_base") + 'clozeeditor/dialog.php?lang=' + lang ,
-                                    width: 620 + parseInt(ed.getLang('clozeeditor.delta_width', 0)),
-                                    height: 360 + parseInt(ed.getLang('clozeeditor.delta_height', 0)),
-                                    inline: true
+                                    file : ed.getParam("moodle_plugin_base") + 'clozeeditor/dialog.php?lang=' + lang ,
+                                    width : 620 + parseInt(ed.getLang('clozeeditor.delta_width', 0)),
+                                    height : 360 + parseInt(ed.getLang('clozeeditor.delta_height', 0)),
+                                    inline : true
                                 }, {
-                                    plugin_url: url, // Plugin absolute URL.
+                                    plugin_url : url, // Plugin absolute URL.
                                 });
                         });
 
                         // Register clozeeditor button.
                         ed.addButton('clozeeditor', {
-                            title: 'clozeeditor.desc',
-                            cmd: 'mceclozeeditor',
-                            image: url + '/img/cloze_editor.png'
+                            title : 'clozeeditor.desc',
+                            cmd : 'mceclozeeditor',
+                            image : url + '/img/cloze_editor.png'
                         });
 
                         // Add a node change handler, selects the button in the UI when a image is selected.
@@ -73,11 +73,11 @@
                  */
             getInfo : function() {
                         return {
-                            longname: 'clozeeditor plugin',
-                            author: 'Andreas Glombitza/Achim Skuta',
-                            authorurl: 'http://tinymce.moxiecode.com',
-                            infourl: 'http://wiki.moxiecode.com/index.php/TinyMCE:Plugins/clozeeditor',
-                            version: "2.4"
+                            longname : 'clozeeditor plugin',
+                            author : 'Andreas Glombitza/Achim Skuta',
+                            authorurl : 'http://tinymce.moxiecode.com',
+                            infourl : 'http://wiki.moxiecode.com/index.php/TinyMCE:Plugins/clozeeditor',
+                            version : "2.4"
                 };
             }
         });
